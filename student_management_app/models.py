92cc7c450db358eca06651cd5045799894d88120
Victor Campelo <victor_campelo@outlook.com>
2020-04-18 18:00:50 -0300

    feat: add models.Manager() on each created class

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -2,51 +2,57 @@ from django.db import models
 
 # Create your models here.
 class Root(models.Model):
-    root_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=255)
     email = models.EmailField()
     password = models.CharField(max_length=255)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
 
 class Director(models.Model):
-    director_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=255)
     email = models.EmailField()
     password = models.CharField(max_length=255)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
 
 class Class(models.Model):
-    class_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     email = models.EmailField()
     password = models.CharField(max_length=255)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now_add=True)
-    director = models.F
+    director_id = models.ForeignKey(Director, on_delete=models.DO_NOTHING)
+    objects = models.Manager()
 
 class Teacher(models.Model):
-    teacher_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=255)
     email = models.EmailField()
     password = models.CharField(max_length=255)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
 
 class Subject(models.Model):
-    subject_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now_add=True)
-    classe = models.ForeignKey(Class, on_delete=models.CASCADE)
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    class_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+    teacher_id = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
+    objects = models.Manager()
 
 class Student(models.Model):
-    student_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=255)
     email = models.EmailField()
     password = models.CharField(max_length=255)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now_add=True)
     subject = models.ManyToManyField(Subject)
+    objects = models.Manager()

ba92ed81ab14e7cf09c161cc01981ff5ae0c5160
Victor Campelo <victor_campelo@outlook.com>
2020-04-18 22:41:13 -0300

    feat: create a Custom User and remove pattern fields

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -1,29 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    user_type_data = ((1, 'root'),(2, 'director'),(3, 'student'))
+    user_type = models.CharField(default=1, choices=user_type_data, max_length=10)
+
 class Root(models.Model):
     id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=255)
-    email = models.EmailField()
-    password = models.CharField(max_length=255)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now_add=True)
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     objects = models.Manager()
 
 class Director(models.Model):
     id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=255)
-    email = models.EmailField()
-    password = models.CharField(max_length=255)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now_add=True)
+    session_start=models.DateField()
+    session_end=models.DateField()
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     objects = models.Manager()
 
 class Class(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    email = models.EmailField()
-    password = models.CharField(max_length=255)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now_add=True)
     director_id = models.ForeignKey(Director, on_delete=models.DO_NOTHING)
@@ -55,4 +56,5 @@ class Student(models.Model):
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now_add=True)
     subject = models.ManyToManyField(Subject)
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     objects = models.Manager()

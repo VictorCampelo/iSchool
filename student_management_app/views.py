ceceff3f7b158522c649a6abf0e764492a933f07
Victor Campelo <victor_campelo@outlook.com>
2020-04-18 23:44:02 -0300

    fix: adjust to test

--- student_management_app/views.py
+++ student_management_app/views.py
@@ -16,13 +16,14 @@ def showLoginPage(request):
 
 def doLogin(request):
     if request.method!="POST":
+        print(request)
         return HttpResponse("<h2>Method Not Allowed</h2>")
     else:
         user=EmailBackEnd.authenticate(request,username=request.POST.get("email"),password=request.POST.get("password"))
         if user!=None:
             login(request,user)
             if user.user_type=="1":
-                return HttpResponseRedirect('/admin_home')
+                return HttpResponse("EMAIL:"+request.POST.get("email")+" pass: "+request.POST.get("password"))
             elif user.user_type=="2":
                 return HttpResponseRedirect(reverse("director_home"))
             else:
@@ -30,3 +31,13 @@ def doLogin(request):
         else:
             messages.error(request,"Invalid Login Details")
             return HttpResponseRedirect("/")
+
+def GetUserDetails(request):
+    if request.user!=None:
+        return HttpResponse("User : "+request.user.email+" usertype : "+str(request.user.user_type))
+    else:
+        return HttpResponse("Please Login First")
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect("/")

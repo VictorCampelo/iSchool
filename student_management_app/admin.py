6a5c4f811ae2e455cdd14246e3e745796dd661aa
Victor Campelo <victor_campelo@outlook.com>
2020-04-28 18:47:52 -0300

    feat/fix: add new users, routes and alter model database to correct vesion without err at Class table

--- student_management_app/admin.py
+++ student_management_app/admin.py
@@ -12,9 +12,14 @@ class UserModel(UserAdmin):
 
 def admin_home(request):
     return render(request, "admin_template/admin_home.html")
-
 def add_admin(request):
     return render(request, "admin_template/add_admin.html")
+def add_director(request):
+    return render(request, "admin_template/add_director.html")
+def add_student(request):
+    return render(request, "admin_template/add_student.html")
+def add_teacher(request):
+    return render(request, "admin_template/add_teacher.html")
 
 def save_admin(request):
     if request.method!="POST":
@@ -33,5 +38,60 @@ def save_admin(request):
         except:
             messages.error(request,"Failed to Add Admin")
             return HttpResponseRedirect(reverse("add_admin"))
+
+def save_director(request):
+    if request.method!="POST":
+        return HttpResponse("Method Not Allowed")
+    else:
+        first_name=request.POST.get("first_name")
+        last_name=request.POST.get("last_name")
+        username=request.POST.get("username")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        try:
+            user=CustomUser.objects.create_user(username=username,password=password,email=email,last_name=last_name,first_name=first_name,user_type=2, is_superuser=0)
+            # user.director.address=address
+            user.save()
+            messages.success(request,"Successfully Added Director")
+            return HttpResponseRedirect(reverse("add_director"))
+        except:
+            messages.error(request,"Failed to Add Director")
+            return HttpResponseRedirect(reverse("add_director"))
+
+def save_student(request):
+    if request.method!="POST":
+        return HttpResponse("Method Not Allowed")
+    else:
+        first_name=request.POST.get("first_name")
+        last_name=request.POST.get("last_name")
+        username=request.POST.get("username")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        try:
+            user=CustomUser.objects.create_user(username=username,password=password,email=email,last_name=last_name,first_name=first_name,user_type=4, is_superuser=0)
+            user.save()
+            messages.success(request,"Successfully Added Student")
+            return HttpResponseRedirect(reverse("add_student"))
+        except:
+            messages.error(request,"Failed to Add Student")
+            return HttpResponseRedirect(reverse("add_student"))
+
+def save_teacher(request):
+    if request.method!="POST":
+        return HttpResponse("Method Not Allowed")
+    else:
+        first_name=request.POST.get("first_name")
+        last_name=request.POST.get("last_name")
+        username=request.POST.get("username")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        try:
+            user=CustomUser.objects.create_user(username=username,password=password,email=email,last_name=last_name,first_name=first_name,user_type=3, is_superuser=0)
+            user.save()
+            messages.success(request,"Successfully Added Teacher")
+            return HttpResponseRedirect(reverse("add_teacher"))
+        except:
+            messages.error(request,"Failed to Add Teacher")
+            return HttpResponseRedirect(reverse("add_teacher"))
 
 admin.site.register(CustomUser, UserModel)
--- student_management_app/models.py
+++ student_management_app/models.py
@@ -5,7 +5,7 @@ from django.dispatch import receiver
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    user_type_data = ((1, 'root'),(2, 'director'),(3, 'student'))
+    user_type_data = ((1, 'root'),(2, 'director'),(3, 'teacher'),(4, 'student'))
     user_type = models.CharField(default=1, choices=user_type_data, max_length=10)
 
 class Root(models.Model):
@@ -19,26 +19,30 @@ class Director(models.Model):
     id = models.AutoField(primary_key=True)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now_add=True)
-    session_start=models.DateField()
-    session_end=models.DateField()
     admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     objects = models.Manager()
 
-class Class(models.Model):
+class School(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+class SchoolClass(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now_add=True)
+    school_id = models.ForeignKey(School, on_delete=models.CASCADE)
     director_id = models.ForeignKey(Director, on_delete=models.DO_NOTHING)
     objects = models.Manager()
 
 class Teacher(models.Model):
     id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=255)
-    email = models.EmailField()
-    password = models.CharField(max_length=255)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now_add=True)
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     objects = models.Manager()
 
 class Subject(models.Model):
@@ -46,15 +50,12 @@ class Subject(models.Model):
     name = models.CharField(max_length=255)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now_add=True)
-    class_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+    class_id = models.ForeignKey(SchoolClass, on_delete=models.CASCADE)
     teacher_id = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
     objects = models.Manager()
 
 class Student(models.Model):
     id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=255)
-    email = models.EmailField()
-    password = models.CharField(max_length=255)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now_add=True)
     subject = models.ManyToManyField(Subject)
@@ -70,6 +71,8 @@ def create_user_profile(sender,instance,created,**kwargs):
         if instance.user_type==2:
             Director.objects.create(admin=instance)
         if instance.user_type==3:
+            Teacher.objects.create(admin=instance)
+        if instance.user_type==4:
             Student.objects.create(admin=instance)
 
 @receiver(post_save,sender=CustomUser)
@@ -79,4 +82,6 @@ def save_user_profile(sender,instance,**kwargs):
     if instance.user_type==2:
         instance.director.save()
     if instance.user_type==3:
+        instance.teacher.save()
+    if instance.user_type==4:
         instance.student.save()

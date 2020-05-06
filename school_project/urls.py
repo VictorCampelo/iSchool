d49b3b2ed8ddc97cd5e9cad6cb4e2cf072667849
Victor Campelo <victor_campelo@outlook.com>
2020-05-05 21:57:24 -0300

    feat: add class path

--- school_project/urls.py
+++ school_project/urls.py
@@ -26,15 +26,19 @@ urlpatterns = [
     path('get_user_details', views.GetUserDetails),
     path('logout_user', views.logout_user,name="logout"),
     path('admin_home',admin.admin_home,name="admin_home"),
+    
     path('add_admin',admin.add_admin,name="add_admin"),
     path('add_director',admin.add_director,name="add_director"),
     path('add_student',admin.add_student,name="add_student"),
     path('add_teacher',admin.add_teacher,name="add_teacher"),
     path('add_school',admin.add_school,name="add_school"),
+    path('add_class',admin.add_class,name="add_class"),
 
     path('save_admin',admin.save_admin,name="save_admin"),
     path('save_director',admin.save_director,name="save_director"),
     path('save_teacher',admin.save_teacher,name="save_teacher"),
     path('save_student',admin.save_student,name="save_student"),
     path('save_school',admin.save_school,name="save_school"),
+    path('save_class',admin.save_class,name="save_class"),
+    
 ]+static(settings.STATIC_URL, documents_root=settings.STATIC_ROOT)
